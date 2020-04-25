a5d22dad8a6f870acc8be9d67c842eebc91e98be
al <al@Batman.attlocal.net>
2020-04-24 20:26:17 -0500

    setup searching with built out backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,16 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import { useSelector, useDispatch } from 'react-redux';
 
 /* styles */
-import { Container, Content } from './styles/global/structure';
-import { MainHeading } from './styles/global/type';
-import { MainSearch } from './styles/global/forms';
+import { Container } from './styles/global/structure';
 
-/* assets */
-import Search from './assets/Search.svg';
+/* components */
+import SearchView from './components/SearchView';
 
 function App() {
 
-  /* redux config */
-  const dispatch = useDispatch();
-
-  /* configure react-hook-form */
-  const { register, errors, handleSubmit } = useForm();
-
-  /* handle form submission */
-  const onSubmit = (data) => {
-    console.log(data);
-    dispatch({ type: 'START_SEARCH' });
-    dispatch({ type: 'SEARCH_SUCCESS', payload: data });
-  }
-
   return (
     <Container>
-      <Content>
-        <MainHeading>Safe Fur Dogs</MainHeading>
-
-        <MainSearch onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            name="query"
-            ref={register()}
-            autoComplete="off"
-          />
-          <button type="submit"><img src={Search} alt="Search" /></button>
-        </MainSearch>
-      </Content>
+      <SearchView />
     </Container>
   );
 }
