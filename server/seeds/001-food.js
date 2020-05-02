9154462283c05efa3a4e12cc045a545fd238eb5e
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-05-01 19:02:04 -0500

    desktop

--- server/seeds/001-food.js
+++ server/seeds/001-food.js
@@ -9,29 +9,46 @@ exports.seed = function (knex) {
           food: 'chocolate',
           safe: false,
           img: 'http://safefurdogs.com/img/chocolate.svg',
-          summary: 'This is an example summary. It can be changed.',
-          resources: []
+          summary: 'Chocolate is toxic to dogs and could cause a medical emergency depending on the amount ingested.',
+          resources: [
+            ['What to Do if Your Dog Eats Chocolate', 'https://www.akc.org/expert-advice/health/what-to-do-if-your-dog-ate-chocolate/'],
+            ['4 Types of Chocolate, and How They Impact Dogs', 'https://www.petmd.com/dog/nutrition/4-types-chocolate-and-how-they-impact-dogs'],
+            ['Chocolate Poisoning in Dogs', 'https://vcahospitals.com/know-your-pet/chocolate-poisoning-in-dogs'],
+            ['What to do if Your Dog Eats Chocolate', 'https://www.vets-now.com/pet-care-advice/dog-eaten-chocolate/']
+          ]
         },
         {
           food: 'chicken',
           safe: true,
           img: 'http://safefurdogs.com/img/chicken.svg',
-          summary: 'This is an example summary. It can be changed.',
-          resources: []
+          summary: 'Chicken can be a great source of protein for dogs, and can even be substituted for or added to his or her regular meal.',
+          resources: [
+            ['Can Dogs Eat Chicken', 'https://www.akc.org/expert-advice/nutrition/can-dogs-eat-chicken/'],
+            ['Healthy Food Checklist: Chicken for Dogs', 'https://www.petmd.com/dog/nutrition/healthy-foods-checklist-chicken-dogs'],
+            ['28 Foods Not To Feed Your Dog (And A List Of Those You Can)', 'https://www.caninejournal.com/foods-not-to-feed-dog/']
+          ]
         },
         {
           food: 'eggs',
           safe: true,
           img: 'http://safefurdogs.com/img/eggs.svg',
-          summary: 'This is an example summary. It can be changed.',
-          resources: []
+          summary: 'The high protein content, essential amino acids, and fatty acids in eggs make them an excellent treat or dietary supplement for dogs.',
+          resources: [
+            ['Can Dogs Eat Eggs?', 'https://www.akc.org/expert-advice/nutrition/can-dogs-eat-eggs/'],
+            ['Can Dogs Eat Eggs?', 'https://www.purina.com/articles/dog/nutrition/can-dogs-eat-eggs']
+          ]
         },
         {
           food: 'grapes',
           safe: false,
           img: 'http://safefurdogs.com/img/grapes.svg',
-          summary: 'This is an example summary. It can be changed.',
-          resources: []
+          summary: 'Grapes and their raisin counterpart are highly toxic to dogs.',
+          resources: [
+            ['Are Grapes Bad for Dogs?', 'https://www.akc.org/expert-advice/nutrition/can-dogs-eat-grapes/'],
+            ['Can Dogs Eat Grapes?', 'https://www.purina.com/articles/dog/nutrition/can-dogs-eat-grapes'],
+            ['Grape and Raisin Poisoning in Dogs', 'https://www.petmd.com/dog/emergency/digestive/e_dg_grape_raisin_toxicity'],
+            ['How Toxic Are Grapes for Dogs', 'https://www.petful.com/pet-health/how-toxic-are-grapes-for-dogs/']
+          ]
         }
       ]);
     });
