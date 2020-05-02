bcbcdd492a47f307561e5b980af84274caa9fb8f
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-05-02 16:53:27 -0500

    user reducer removed for now until combined reducers issue can be solved

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import { userReducer } from './userReducer';
 import { searchReducer } from './searchReducer';
 
 export default combineReducers({
-    userReducer,
     searchReducer
 });
