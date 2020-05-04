9fb2c8a5afd596cdc27d698e3bcc3b8298a89c0f
al <al@Batman.attlocal.net>
2020-05-03 22:20:35 -0500

    some moving around. breaking server into different repo for easier deployment

--- server/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-
-const server = require('./api/server.js');
-
-const PORT = process.env.PORT;
-server.listen(PORT, () => {
-    console.log('Server online...');
-})
