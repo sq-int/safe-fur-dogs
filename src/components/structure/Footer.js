acd984f2e5d3f7b4d6af28715610c2568d4daa0c
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-05-06 15:10:44 -0500

    removed footer for now

--- src/components/structure/Footer.js
+++ src/components/structure/Footer.js
@@ -6,7 +6,7 @@ import { FooterContainer } from '../../styles/footer';
 export default function Footer() {
     return (
         <FooterContainer>
-            <span><a target="_blank" rel="noopener noreferrer" href="http://alexcooter.com">alexlc 2020.</a></span>
+
         </FooterContainer>
     )
 }
