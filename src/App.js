9b84929cac43af1007f0d73f6e11c956d07c7989
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-05-07 12:40:08 -0500

    removed tools for now

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import SearchView from './components/SearchView';
 import Food from './components/Food';
 import Header from './components/structure/Header';
 import Footer from './components/structure/Footer';
-import Build from './components/structure/Build';
-import Submit from './components/structure/Submit';
 import PrivateRoute from './components/structure/PrivateRoute';
 
 function App() {
@@ -29,11 +27,6 @@ function App() {
         <Route exact path="/:food">
           <Food />
         </Route>
-        <Route path={process.env.REACT_APP_BUILD}>
-          <Build />
-        </Route>
-
-        <PrivateRoute exact path={process.env.REACT_APP_FOOD} component={Submit} />
       </Switch>
 
       <Footer />
