806d2584365c50dd061e8352ce23221cfefc893c
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-09-24 17:51:17 -0500

    Thank You component after successful missing food submission built with reusable styles. Component redirects user back home after 4 seconds.

--- src/components/alerts/ThankYou.js
+++ src/components/alerts/ThankYou.js
@@ -1,14 +1,42 @@
 // react
-import React from "react";
+import React, { useEffect } from "react";
 // react-router-dom
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+// styles
+import { Contain, Block } from "../../styles/global/structure";
+import { Paragraph } from "../../styles/global/type";
 
 export default function ThankYou() {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push(`/`);
+    }, 4000);
+  }, []);
+
   return (
-    <div>
-      Thank you for submitting a missing item. We will research its safety and
-      make sure it gets added to the database!
-      <Link to="/">Go Home</Link>
-    </div>
+    <Contain
+      width="80%"
+      height="80vh"
+      display="flex"
+      justify="center"
+      align="center"
+    >
+      <Block
+        width="100%"
+        display="flex"
+        direction="column"
+        justify="center"
+        align="center"
+      >
+        <Paragraph fontSize="2.4rem" margin="0 0 2rem 0">
+          Thank you for submitting a missing item. We will research its safety
+          and make sure it gets added to the database!
+        </Paragraph>
+
+        <Paragraph fontSize="1.4rem">Taking you back home...</Paragraph>
+      </Block>
+    </Contain>
   );
 }
