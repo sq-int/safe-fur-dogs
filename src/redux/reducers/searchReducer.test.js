a87953a8b7d21e31ab5c6edf5ccd99a45e975f80
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-09-30 22:08:11 -0500

    More unit tests for searchReducer

--- src/redux/reducers/searchReducer.test.js
+++ src/redux/reducers/searchReducer.test.js
@@ -3,6 +3,15 @@ import { searchReducer } from "./searchReducer";
 import { searchActionTypes } from "../actions/searchActions";
 import { food, expectedState } from "../../data/mocks/searchItemMock";
 
+const initialState = {
+  loading: false,
+  error: "",
+  query: "",
+  limit: false,
+  food: [],
+  suggestions: [],
+};
+
 describe("search reducer", () => {
   test("It should return the initial state", () => {
     const initialState = {
@@ -17,6 +26,19 @@ describe("search reducer", () => {
     expect(searchReducer(undefined, {})).toEqual(initialState);
   });
 
+  test("It should return loading state when starting a search for food", () => {
+    const successAction = {
+      type: searchActionTypes.SEARCH_START,
+    };
+
+    const update = {
+      ...initialState,
+      loading: true,
+    };
+
+    expect(searchReducer(undefined, successAction)).toEqual(update);
+  });
+
   test("It should set food state to successfully found food item", () => {
     const successAction = {
       type: searchActionTypes.FOOD_FOUND,
@@ -24,5 +46,19 @@ describe("search reducer", () => {
     };
 
     expect(searchReducer(undefined, successAction)).toEqual(expectedState);
+  });
+
+  test("It should return the correct state for a failed food item search", () => {
+    const failAction = {
+      type: searchActionTypes.SEARCH_FAIL,
+    };
+
+    const update = {
+      ...initialState,
+      loading: false,
+      error: "That particular food item is not yet in our database.",
+    };
+
+    expect(searchReducer(undefined, failAction)).toEqual(update);
   });
 });
